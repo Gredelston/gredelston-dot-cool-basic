2f9c7ffcf05815fe3a6c8a9cbf58b22ed153a106
Greg Edelston <gredelston@gmail.com>
2019-04-14 19:38:03 -0600

    Add blog post 'Recapping Mystery Hunt 2019, Part 1: Story' from my old blog

--- src/pages/about.js
+++ src/pages/about.js
@@ -12,8 +12,12 @@ class NotFoundPage extends React.Component {
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO title="About Me" />
-        <h1>Greg Edelston</h1>
-        <p>Wow, it's me!</p>
+        <h1>Greg Edelston, Whose Website You&rsquo;re Viewing</h1>
+        <p>The rumors are true. I&rsquo;m Greg, and this is my website.</p>
+        <p>Professionally, I write software at Google in Boulder, Colorado. I use GTD to keep my life organized.</p>
+        <p>Hobbially, my more invested hobbies include improv comedy and studying Zen Buddhism.</p>
+        <p>Other hobbies include making music, writing puzzles, playing board games, and trying to stay moderately fit.</p>
+        <p>Someday I&rsquo;ll put my resume here.</p>
       </Layout>
     )
   }
